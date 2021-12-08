b0660ea5e988686294db9fed96ddfaf8862bb216
Myo Htet <myohtetcu2021@gmail.com>
2021-12-08 10:30:15 +0900

    react-basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+class Item extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Item component heading</h1>
+        <p>item component</p>
+      </div>
+    );
+  }
 }
-
+class App extends React.Component {
+  render() {
+    return <Item />;
+  }
+}
 export default App;
