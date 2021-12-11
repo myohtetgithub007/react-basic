4e70364cd2655e741ba65730518510299120fb9c
Myo Htet <myohtetcu2021@gmail.com>
2021-12-11 21:51:46 +0900

    react function componennt

--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,16 @@
 import React from "react";
-class Header extends React.Component {
-  render() {
-    return (
-      <div style={{ background: "cyan", padding: 10 }}>
-        {this.props.children}
-      </div>
-    );
-  }
-}
-export default Header;
-
-//React Function components
-function Header(props) {
-  return <div>{props.name}</div>;
-}
-export default Header;
-
+//Build in using create react component
+// class Header extends React.Component {
+//   render() {
+//     return <div>Hello</div>;
+//   }
+// }
+// Function component
+// function Header(props) {
+//   return <div>How are you I am good</div>;
+// }
+//Es6 using create react component
 const Header = (props) => {
-  return <div>{props.name}</div>;
+  return <div>{props.name} おはようございます</div>;
 };
-
 export default Header;
