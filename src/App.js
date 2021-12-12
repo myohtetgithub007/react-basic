56ed20a4f42e7757bb7bdbad686bdd9fac512a2b
Myo Htet <myohtetcu2021@gmail.com>
2021-12-12 17:39:53 +0900

    react state and fucntion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import React from "react";
-import Header from "./Header";
+// import Header from "./Header";
+// import Item from "./Item";
+class Item extends React.Component {
+  render() {
+    return (
+      <div>
+        <li>{this.props.name}</li>
+        <li>{this.props.price}</li>
+      </div>
+    );
+  }
+}
 class App extends React.Component {
   state = {
-    name: "orange",
-    color: "red",
+    items: [
+      { id: 1, name: "Apple", price: 0.99 },
+      { id: 1, name: "Orange", price: 1.2 },
+    ],
   };
-  changeColor = () => {
-    this.setState({ color: "green" });
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  add = () => {
+    let id = this.state.items.length + 1;
+    let name = this.nameRef.current.value;
+    let price = this.priceRef.current.value;
+    this.setState({
+      items: [...this.state.items, { id, name, price }],
+    });
   };
   render() {
     return (
       <div>
-        <h1>{this.state.color}</h1>
-        <button type="button" onClick={this.changeColor}>
-          Change Color
-        </button>
-        <Header name="Myo Htet">
-          <p>Good Morning</p>
-        </Header>
+        <h1>Hello React Component</h1>
+        <ul>
+          {this.state.items.map((i) => {
+            return <Item name={i.name} price={i.price}></Item>;
+          })}
+        </ul>
+        <input type="text" ref={this.nameRef} />
+        <input type="text" ref={this.priceRef} />
+        <br></br>
+        <button onClick={this.add}>Add</button>
       </div>
     );
   }
